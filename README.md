70b436289bc504123276f5226148e056d25afe22
marioconsalvo <marioconsalvo1@gmail.com>
2019-07-27 00:34:35 +0200

    Base app init

---
+++ README.md
@@ -0,0 +1,51 @@
+TODO TASK:
+[x] IMPLEMENT COUNT DOWN LOGIC
+    [x] ADD +/- LOGIC TO THE TIMER
+[x] IMPLEMENT CUSTOM EVENT 
+[x] ADD REGRESS BAR - Add $varible which calculate the percentage on every update - Find a way to update the progress 
+[ ] COMPONETIZE THE APP
+[x] ADD SOME STYLE
+[x] SEND PUSH NOTIFICATION WHEN TIME IS OUT
+[ ] HOST ON GITHUB
+    PLUS:
+    [ ] ADD SETTINGS PAGE 
+    [ ] PERSIST THE USER SETTINGS TO LOCAL STORAGE
+
+GOALS:
+[ ] CONFIGURE THE APP TO PERFORM AS A PWA
+
+
+
+
+## Deploying to the web
+
+### With [now](https://zeit.co/now)
+
+Install `now` if you haven't already:
+
+```bash
+npm install -g now
+```
+
+Then, from within your project folder:
+
+```bash
+now
+```
+
+As an alternative, use the [Now desktop client](https://zeit.co/download) and simply drag the unzipped project folder to the taskbar icon.
+
+### With [surge](https://surge.sh/)
+
+Install `surge` if you haven't already:
+
+```bash
+npm install -g surge
+```
+
+Then, from within your project folder:
+
+```bash
+npm run build
+surge public
+```
---
+++ src/App.svelte
@@ -0,0 +1,277 @@
+<script>
+  let timerSecondBar = 0;
+  let timerPercentageBar = 0;
+  let min = {
+    now: 0,
+    restart: 0
+  };
+  let sec = {
+    now: 0,
+    restart: 0
+  };
+  const timer = {
+    isPlaying: false
+  };
+  let intervalID;
+  const finishEvent = new Event("timeout");
+  $: {
+    timerSecondBar = min.restart * 60 + sec.restart;
+    timerPercentageBar = 100 / timerSecondBar;
+  }
+  let len = 100;
+  let notificationPermission = "denied";
+  let notificationFlag = false;
+
+  function startTimer(event, time) {
+    timer.isPlaying = true;
+    setIntervalWrap();
+  }
+
+  function setIntervalWrap() {
+    intervalID = setInterval(() => {
+      if (sec.now > 0 && min.now >= 0) {
+        sec.now -= 1;
+        refreshProgressBar();
+      }
+      if (sec.now == 0 && min.now > 0) {
+        min.now -= 1;
+        sec.now = 59;
+        refreshProgressBar();
+      } else if (sec.now <= 0 && min.now <= 0) {
+        clearInterval(intervalID);
+        document.dispatchEvent(finishEvent);
+        setTimeout(() => {
+          timer.isPlaying = false;
+          sec.now = sec.restart;
+          min.now = min.restart;
+          len = 100;
+        }, 2000);
+      }
+    }, 1000);
+  }
+
+  function logger(msg) {
+    console.log(msg);
+    console.log("TIMER " + JSON.stringify(timer));
+    console.log("MIN " + JSON.stringify(min));
+    console.log("SEC " + JSON.stringify(sec));
+    console.log(timerPercentageBar + " " + timerSecondBar);
+  }
+
+  function stopTimer() {
+    logger("stop timer call");
+    timer.isPlaying = false;
+    clearInterval(intervalID);
+  }
+
+  function increaseTimerMinutes() {
+    if (!timer.isPlaying) {
+      min.now += 1;
+      alignMinutes();
+    }
+  }
+  function decreaseTimerMinutes() {
+    if (!timer.isPlaying && min.now > 0) {
+      min.now -= 1;
+      alignMinutes();
+    }
+  }
+  function increaseTimerSeconds() {
+    if (!timer.isPlaying && sec.now < 59) {
+      sec.now += 1;
+      alignSeconds();
+    }
+  }
+  function decreaseTimerSeconds() {
+    if (!timer.isPlaying && sec.now > 0) {
+      sec.now -= 1;
+      alignSeconds();
+    }
+  }
+
+  function resetTimer() {
+    min.now = min.restart = 0;
+    sec.now = sec.restart = 0;
+    stopTimer();
+    len = 100;
+  }
+
+  function alignSeconds() {
+    sec.restart = sec.now;
+  }
+  function alignMinutes() {
+    min.restart = min.now;
+  }
+  function refreshProgressBar() {
+    len -= timerPercentageBar;
+    if (len < 0) len = 0;
+  }
+
+  function enableNotification() {
+    if (notificationPermission === "denied" && !notificationFlag) {
+      Notification.requestPermission().then(function(result) {
+        notificationPermission = result;
+      });
+    }
+  }
+
+  document.addEventListener("timeout", e => {
+    let ofs = 0;
+    let backgroundFlash = setInterval(function() {
+      document.body.style.background =
+        "rgba(" +
+        Math.floor(Math.random() * 256) +
+        "," +
+        Math.floor(Math.random() * 256) +
+        "," +
+        Math.floor(Math.random() * 256) +
+        "," +
+        Math.abs(Math.sin(ofs)) +
+        ")";
+      ofs += 0.05;
+    }, 10);
+    setTimeout(() => {
+      clearInterval(backgroundFlash);
+      document.body.style.background = "transparent";
+    }, 1900);
+    if (notificationPermission === "granted" && notificationFlag) {
+      const text = "Hey your time is out !!";
+      const img = "https://img.icons8.com/clouds/100/000000/timer.png";
+      var notification = new Notification("To do list", {
+        body: text,
+        icon: img
+      });
+      setTimeout(notification.close.bind(notification), 10000);
+    }
+  });
+</script>
+
+<style>
+  .container * {
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  h1,
+  h2 {
+    color: purple;
+    display: inline-block;
+  }
+
+  button {
+    border: none;
+    background-color: darksalmon;
+  }
+
+  .container-crono {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .row {
+    flex: 100%;
+    text-align: center;
+  }
+
+  .progressbar {
+    background-color: yellow;
+    padding: 3px;
+  }
+
+  .progressbar > div {
+    background-color: violet;
+    height: 20px;
+  }
+
+  .checkbox {
+    padding-top: 10px;
+  }
+
+  .whited {
+    color: white;
+  }
+
+  .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+  }
+
+  .crono {
+    font-size: 350%;
+  }
+
+  .adjust-time {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .dontknow {
+    display: flex;
+  }
+
+  a {
+    color: hotpink;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .container-crono {
+      width: 720px;
+      margin-right: auto;
+      margin-left: auto;
+    }
+  }
+</style>
+
+<div class="container-crono">
+  <div class="row">
+    <h1 class="crono">{min.now.toString().padStart(2, 0)}</h1>
+    <h1 class="crono">:</h1>
+    <h1 class="crono">{sec.now.toString().padStart(2, 0)}</h1>
+  </div>
+  <div class="row">
+    <div class="progressbar">
+      <div style="width: {len}%" />
+    </div>
+  </div>
+  <div class="row adjust-time">
+    <div class="">
+      <h1 on:click={increaseTimerMinutes}>+</h1>
+      <div />
+      <h2>MINUTES</h2>
+      <div />
+      <h1 on:click={decreaseTimerMinutes}>-</h1>
+    </div>
+    <div class="">
+      <h1 on:click={increaseTimerSeconds}>+</h1>
+      <div />
+      <h2>SECONDS</h2>
+      <div />
+      <h1 on:click={decreaseTimerSeconds}>-</h1>
+    </div>
+  </div>
+  {#if timer.isPlaying}
+    <button on:click={stopTimer}>PAUSE</button>
+  {:else}
+    <button on:click={startTimer}>START</button>
+  {/if}
+  <button on:click={resetTimer}>RESET</button>
+  <div class="row dontknow">
+    <input
+      type="checkbox"
+      bind:checked={notificationFlag}
+      on:click={enableNotification} />
+    <p class="whited">Check this fancy box if you want an ugly notification</p>
+  </div>
+
+  <div class="footer whited">
+    c r e a t e d b y
+    <a href="https://github.com/co5m0/uglytimer">C O 5 M O</a>
+  </div>
+</div>
